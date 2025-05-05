ea32ec6dd066a7a665e83444b380947fb113abd3
SRISWETHA RAVINDRAN <126751897+Sweee2012@users.noreply.github.com>
2025-05-05 17:16:10 +0530

    Add files via upload

---
+++ Chatbot.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage
+
+# Direct API key (secure this in real apps)
+api_key = "AIzaSyAvgmMD3PvEtHs28RmzJ4c7lxRhOex4g4Y"
+
+# Gemini response function
+def get_gemini_response(input_text):
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.0-flash",
+        google_api_key=api_key,
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2
+    )
+    message = [HumanMessage(content=input_text)]
+    response = llm.invoke(message)
+    return response.content
+
+# Streamlit UI
+st.set_page_config(page_title="🤖 Chatipie", page_icon="💬")
+st.title("💬 Chatipie - Ask Anything!")
+
+st.markdown("Ask me a question and get a smart answer using **Gemini 2.0 Flash**.")
+
+input = st.text_input("Enter your question:", key="input")
+
+if st.button("Generate"):
+    if input:
+        response = get_gemini_response(input)
+        st.subheader("🧠 Gemini Response:")
+        st.write(response)
+    else:
+        st.warning("Please enter a question before clicking")
